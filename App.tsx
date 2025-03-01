fe9ed761968073a17c7c6c874aa70470c8c60271
kdn0325 <dn10003@gmail.com>
2025-03-02 01:50:14 +0900

    Refactor App component to display device information with buttons for battery level, device model, IP address, uptime, and Android version

--- App.tsx
+++ App.tsx
@@ -1,118 +1,66 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import React, {useState, useEffect} from 'react';
+import {View, Text, Button, StyleSheet} from 'react-native';
+import NativeGetDeviceInfo from './specs/NativeGetDeviceInfo';
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+const App = () => {
+  const [value, setValue] = useState<string | null>('');
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
-
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  const getBatteryLevel = async () => {
+    const data = await NativeGetDeviceInfo?.getBatteryLevel();
+    setValue(data ?? '');
   };
 
+  const getDeviceModel = async () => {
+    const data = await NativeGetDeviceInfo?.getDeviceModel();
+    setValue(data ?? '');
+  };
+
+  const getDeviceIpAddress = async () => {
+    const data = await NativeGetDeviceInfo?.getDeviceIpAddress();
+    setValue(data ?? '');
+  };
+
+  const getDeviceUptime = async () => {
+    const data = await NativeGetDeviceInfo?.getDeviceUptime();
+    setValue(data ?? '');
+  };
+
+  const getAndroidVersion = async () => {
+    const data = await NativeGetDeviceInfo?.getAndroidVersion();
+    setValue(data ?? '');
+  };
+
+  useEffect(() => {
+    getBatteryLevel();
+  }, []);
+
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+    <View style={styles.container}>
+      <Text style={styles.title}>{value}</Text>
+      <View style={styles.buttonContainer}>
+        <Button title={'배터리 잔량 확인'} onPress={getBatteryLevel} />
+      </View>
+      <View style={styles.buttonContainer}>
+        <Button title={'기기 모델 확인'} onPress={getDeviceModel} />
+      </View>
+      <View style={styles.buttonContainer}>
+        <Button title={'IP 주소 확인'} onPress={getDeviceIpAddress} />
+      </View>
+      <View style={styles.buttonContainer}>
+        <Button title={'기기 가동 시간 확인'} onPress={getDeviceUptime} />
+      </View>
+      <View style={styles.buttonContainer}>
+        <Button title={'안드로이드 버전 확인'} onPress={getAndroidVersion} />
+      </View>
+    </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
+  container: {flex: 1, backgroundColor: '#f5f5f5'},
+  title: {fontSize: 24, fontWeight: 'bold', marginBottom: 20},
+  taskTitle: {fontSize: 18},
+  buttonContainer: {marginBottom: 20},
 });
 
 export default App;
